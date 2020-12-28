ba9d5d073281b0a56ebe497093a5ddd0e0cc5911
Patrick Jenifer <pjuofa95@gmail.com>
2020-12-28 14:44:05 -0700

    setup of app

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+function App() {
+  return (
+    <table>
+        <th>Round</th>
+        <th>Pick 1</th>
+        <th>Pick 2</th>
+        <th>Pick 3</th>
+        <th>Pick 4</th>
+        <th>Pick 5</th>
+        <th>Pick 6</th>
+        <th>Pick 7</th>
+        <th>Pick 8</th>
+        <th>Pick 9</th>
+      <tr>
+        <td>1</td>
+        <td>Patrick</td>
+        <td>John</td>
+        <td>Matt</td>
+        <td>Unknown</td>
+        <td>Kaden</td>
+        <td>Scott</td>
+        <td>Ryan</td>
+        <td>Brian</td>
+        <td>AJ</td>
+        <td>Stephen</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Patrick</td>
+        <td>John</td>
+        <td>Matt</td>
+        <td>Unknown</td>
+        <td>Kaden</td>
+        <td>Scott</td>
+        <td>Ryan</td>
+        <td>Brian</td>
+        <td>AJ</td>
+        <td>Stephen</td>
+      </tr>
+    </table>
+  );
+}
+
+export default App;
